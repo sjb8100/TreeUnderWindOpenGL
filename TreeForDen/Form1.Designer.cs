9368b24ed1c4cbde84b111f68d99620f3382d341
Egor Ryzhukhin <egor.ryzhukhin@gmail.com>
2018-05-13 23:34:49 +0300

    created sphere

--- TreeForDen/Form1.Designer.cs
+++ TreeForDen/Form1.Designer.cs
@@ -28,42 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.simpleOpenGlControl1 = new Tao.Platform.Windows.SimpleOpenGlControl();
+            this.SOGIControl = new Tao.Platform.Windows.SimpleOpenGlControl();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // simpleOpenGlControl1
+            // SOGIControl
             // 
-            this.simpleOpenGlControl1.AccumBits = ((byte)(0));
-            this.simpleOpenGlControl1.AutoCheckErrors = false;
-            this.simpleOpenGlControl1.AutoFinish = false;
-            this.simpleOpenGlControl1.AutoMakeCurrent = true;
-            this.simpleOpenGlControl1.AutoSwapBuffers = true;
-            this.simpleOpenGlControl1.BackColor = System.Drawing.Color.Black;
-            this.simpleOpenGlControl1.ColorBits = ((byte)(32));
-            this.simpleOpenGlControl1.DepthBits = ((byte)(16));
-            this.simpleOpenGlControl1.Location = new System.Drawing.Point(9, 7);
-            this.simpleOpenGlControl1.Name = "simpleOpenGlControl1";
-            this.simpleOpenGlControl1.Size = new System.Drawing.Size(432, 427);
-            this.simpleOpenGlControl1.StencilBits = ((byte)(0));
-            this.simpleOpenGlControl1.TabIndex = 0;
+            this.SOGIControl.AccumBits = ((byte)(0));
+            this.SOGIControl.AutoCheckErrors = false;
+            this.SOGIControl.AutoFinish = false;
+            this.SOGIControl.AutoMakeCurrent = true;
+            this.SOGIControl.AutoSwapBuffers = true;
+            this.SOGIControl.BackColor = System.Drawing.Color.Black;
+            this.SOGIControl.ColorBits = ((byte)(32));
+            this.SOGIControl.DepthBits = ((byte)(16));
+            this.SOGIControl.Location = new System.Drawing.Point(9, 7);
+            this.SOGIControl.Name = "SOGIControl";
+            this.SOGIControl.Size = new System.Drawing.Size(432, 427);
+            this.SOGIControl.StencilBits = ((byte)(0));
+            this.SOGIControl.TabIndex = 0;
+            this.SOGIControl.Load += new System.EventHandler(this.SOGIControl_Load);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(448, 7);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(115, 31);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Визуализировать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(448, 44);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(115, 31);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Выйти";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.ClientSize = new System.Drawing.Size(453, 446);
-            this.Controls.Add(this.simpleOpenGlControl1);
+            this.ClientSize = new System.Drawing.Size(575, 446);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.SOGIControl);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "Form1";
             this.Text = "tree_demo";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private Tao.Platform.Windows.SimpleOpenGlControl simpleOpenGlControl1;
+        private Tao.Platform.Windows.SimpleOpenGlControl SOGIControl;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- TreeForDen/Form1.cs
+++ TreeForDen/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Tao.FreeGlut;
+using Tao.OpenGl;
 
 namespace TreeForDen
 {
@@ -15,6 +17,51 @@ namespace TreeForDen
         public Form1()
         {
             InitializeComponent();
+            SOGIControl.InitializeContexts();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // инициализация Glut 
+            Glut.glutInit();
+            Glut.glutInitDisplayMode(Glut.GLUT_RGB | Glut.GLUT_DOUBLE | Glut.GLUT_DEPTH);
+            // отчитка окна 
+            Gl.glClearColor(255, 255, 255, 1);
+            // установка порта вывода в соответствии с размерами элемента anT 
+            Gl.glViewport(0, 0, SOGIControl.Width, SOGIControl.Height);
+            // настройка проекции 
+            Gl.glMatrixMode(Gl.GL_PROJECTION);
+            Gl.glLoadIdentity();
+            Glu.gluPerspective(45, (float)SOGIControl.Width / (float)SOGIControl.Height, 0.1, 200);
+            Gl.glMatrixMode(Gl.GL_MODELVIEW);
+            Gl.glLoadIdentity();
+            // настройка параметров OpenGL для визуализации 
+            Gl.glEnable(Gl.GL_DEPTH_TEST); 
+        }
+
+        private void SOGIControl_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Gl.glClear(Gl.GL_COLOR_BUFFER_BIT | Gl.GL_DEPTH_BUFFER_BIT);
+            Gl.glLoadIdentity();
+            Gl.glColor3f(1.0f, 0, 0);
+            Gl.glPushMatrix();
+            Gl.glTranslated(0, 0, -6);
+            Gl.glRotated(45, 1, 1, 0);
+
+            Glut.glutWireSphere(2, 32, 32);
+            Gl.glPopMatrix();
+            Gl.glFlush();
+            SOGIControl.Invalidate();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
         }
     }
 }
